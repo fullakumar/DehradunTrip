5eba493c4812a946e6929ee1f61c4e17c33e1fb9
Fulla Kumar <fulla420840@gmail.com>
2025-07-23 15:44:30 +0530

    changes in showData/page.jsx

--- app/serverActions/contact.action.jsx
+++ app/serverActions/contact.action.jsx
@@ -1,5 +1,6 @@
 "use server"
 import axios from "axios";
+import { revalidateTag } from "next/cache";
 
 export const contactAction = async (formData) =>{
     const data = Object.fromEntries(formData.entries());
@@ -9,17 +10,14 @@ export const contactAction = async (formData) =>{
 async function sendData(data) {
   try {
     const response = await axios.post('https://trip-y1a7.onrender.com/createTrip', 
-        data,{
-  headers: {
-    "Content-Type": "application/json",
-  },
-}
+        data
     );
     console.log('Success:', response.data);
   } catch (error) {
     console.error('Error:', error);
   }
 }
+
 
 
 
